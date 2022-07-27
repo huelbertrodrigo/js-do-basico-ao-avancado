cd01c7260a2419d7ab693e6cb05085edb245ff78
Huelbert Rodrigo <rodrigo@scontti.com.br>
2022-07-27 10:14:39 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,14 @@
+let nome = 'Huelbert' // const da erro pois não pode mudar o valor
+
+console.log(nome)
+
+nome = 'Rodrigo'
+
+console.log(nome)
+
+/*
+ * não pode em variaveis começar com numero (9nome)
+ * unicos caracteres especiais ($ ou _)
+ * utilizar camelCase
+ * observar as palavras reservadas
+ */
