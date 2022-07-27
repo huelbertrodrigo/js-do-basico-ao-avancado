41dee049abed7d4c7544f0a337c6b698f7091cf8
Huelbert Rodrigo <rodrigo@scontti.com.br>
2022-07-27 10:14:32 -0300

    math

---
+++ math.js
@@ -0,0 +1,13 @@
+let maior = Math.max(1, 2, 5, 6, 7)
+console.log(maior)
+
+let menor = Math.min(1, 2, 5, 6, 7)
+console.log(menor)
+
+let arredondar1 = Math.round(1.55555)
+let arredondar2 = Math.round(1.22222)
+console.log(arredondar1)
+console.log(arredondar2)
+
+let acima = Math.ceil(1.222)
+console.log(acima)
